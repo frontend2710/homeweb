604c8e68941faac7c87f5c2b4ff94ae6496da545
frontend2710 <99738299+frontend2710@users.noreply.github.com>
2022-02-27 11:17:42 +0530

    uploading controller

---
+++ controller.js
@@ -0,0 +1,17 @@
+
+
+// const checkbox = document.getElementById("customSwitches");
+// const map= document.getElementById("map");
+
+// checkbox.addEventListener('checked', function handleClick() {
+//     if (checkbox.checked) {
+//       map.style.display = 'block';
+//     } else {
+//       map.style.display = 'none';
+//     }
+//   });
+
+//   console.log(checkbox.addEventListener);
+
+// onchange toggle event
+
